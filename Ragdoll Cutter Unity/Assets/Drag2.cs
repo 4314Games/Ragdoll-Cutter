decc70ee317a3c464af2361141487b4479a30764
4314Games <4314Games@gmail.com>
2017-03-26 16:36:57 +1100

    Original Files
    Start of the project.

---
+++ Ragdoll Cutter Unity/Assets/Drag2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag2 : MonoBehaviour {
+
+    Vector3 screenPoint, offset;
+    float _lockedYPosition;
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); // I removed this line to prevent centring 
+        _lockedYPosition = screenPoint.y;
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        Cursor.visible = false;
+
+
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        curPosition.x = _lockedYPosition;
+        transform.position = curPosition;
+    }
+
+    void OnMouseUp()
+    {
+        Cursor.visible = true;
+    }
+}
---
+++ Ragdoll Cutter Unity/Assets/Test.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    bool mouse;
+    private Camera FindCamera()
+    {
+        if (GetComponent<Camera>())
+        {
+            return GetComponent<Camera>();
+        }
+
+        return Camera.main;
+    }
+    void Update()
+    {
+
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        var mainCamera = FindCamera();
+
+        // We need to actually hit an object
+        RaycastHit hit = new RaycastHit();
+        if (
+            !Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition).origin,
+                             mainCamera.ScreenPointToRay(Input.mousePosition).direction, out hit, 100,
+                             Physics.DefaultRaycastLayers))
+        {
+            return;
+        }
+        //    if (Input.GetButtonDown("Fire1"))
+        //    {
+        //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //        if (Physics.Raycast(ray)) { }
+
+        //    }
+
+        //    if (mouse) GetComponent<Rigidbody>().MovePosition(Input.mousePosition);
+        //    mouse = false;
+        //}
+        //void OnMouseDrag()
+        //{
+        //    mouse = true;
+        //}
+        //void FixedUpdate()
+        //{
+
+        //}
+    }
+}
